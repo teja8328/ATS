156f179c4f93eb688383a88672411db3f7d83183
teja8328 <135217705+teja8328@users.noreply.github.com>
2024-11-11 14:33:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4799,163 +4799,6 @@ def generate_random_password(length=8):
 #         return jsonify({'message': 'You do not have permission to create recruiter accounts.'})
 
 #--changes here--------------------------------------------------------------------
-# def send_verification_email(new_user, password, verification_link):
-#     html_body = f'''
-#     <html>
-#     <head>
-#         <style>
-#             body {{
-#                 font-family: Arial, sans-serif;
-#                 line-height: 1.6;
-#             }}
-#             .container {{
-#                 max-width: 600px;
-#                 margin: auto;
-#                 padding: 20px;
-#                 border: 1px solid #ddd;
-#                 border-radius: 5px;
-#                 box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-#             }}
-#             h2 {{
-#                 color: #333;
-#             }}
-#             p {{
-#                 color: #555;
-#             }}
-#             ul {{
-#                 list-style-type: none;
-#                 padding: 0;
-#             }}
-#             ul li {{
-#                 background: #f9f9f9;
-#                 margin: 5px 0;
-#                 padding: 10px;
-#                 border: 1px solid #ddd;
-#                 border-radius: 3px;
-#             }}
-#             a {{
-#                 color: #1a73e8;
-#                 text-decoration: none;
-#             }}
-#             a:hover {{
-#                 text-decoration: underline;
-#             }}
-#             .footer {{
-#                 margin-top: 20px;
-#                 font-size: 0.9em;
-#                 color: #888;
-#             }}
-#         </style>
-#     </head>
-#     <body>
-#         <div class="container">
-#             <h2>Hello {new_user.name},</h2>
-#             <p>We are pleased to inform you that your account has been successfully created for the <strong>ATS Makonis Talent Track Pro</strong>. Here are your login credentials:</p>
-#             <ul>
-#                 <li><strong>Username:</strong> {new_user.username}</li>
-#                 <li><strong>Password:</strong> {password}</li>
-#             </ul>
-#             <p>Please note that the verification link will expire after 24 hours.</p>
-#             <p>To verify your account, please click on the following link:</p>
-#             <p><a href="{verification_link}">Verify Your Account</a></p>
-#             <p>After successfully verifying your account, you can access the application using the following link:</p>
-#             <p><a href="https://ats-makonis.netlify.app/">Application Link (Post Verification)</a></p>
-#             <p>If you have any questions or need assistance, please feel free to reach out.</p>
-#             <p>Best regards,</p>
-#             <p><strong>ATS Makonis Talent Track Pro Team</strong></p>
-#             <div class="footer">
-#                 <p>This is an automated message, please do not reply.</p>
-#             </div>
-#         </div>
-#     </body>
-#     </html>
-#     '''
-
-#     msg = Message('Account Verification', sender=config.sender_email, recipients=[new_user.email])
-#     msg.html = html_body
-#     try:
-#         mail.send(msg)
-#     except Exception as e:
-#         print("mail error", str(e))
-#         return f'Failed to send mail: {str(e)}'
-#     return None
-
-# # The signup function
-# @app.route('/signup', methods=['POST'])
-# def signup():
-#     data = request.json
-#     user_id = data.get('user_id')  # Using get method to avoid KeyError
-#     user = User.query.filter_by(id=user_id).first()
-
-#     if not user:
-#         return jsonify({'status': 'error', 'message': 'Invalid user ID or user does not exist.'})
-
-#     user_type = user.user_type
-#     user_name = user.username
-
-#     if user_type == 'management':
-#         username = data.get('username')
-#         name = data.get('name')
-#         email = data.get('email')
-#         user_type = data.get('user_type')
-
-#         # Check if required fields are provided
-#         if not all([username, name, email, user_type]):
-#             return jsonify({'status': 'error', 'message': 'All fields are required'})
-
-#         # Generate a random password
-#         password = generate_random_password()
-#         hashed_password = hashlib.sha256(password.encode()).hexdigest()
-
-#         created_by = user_name
-
-#         existing_user = User.query.filter(or_(User.username == username, User.email == email, User.name == name)).first()
-
-#         if existing_user:
-#             return jsonify({'status': 'error', 'message': 'Account with the same Username, Email, or Name already exists.'})
-
-#         new_user = User(username=username, password=hashed_password, name=name, email=email, user_type=user_type, created_by=created_by)
-        
-#         db.session.add(new_user)
-#         db.session.commit()
-
-#         # Generate a verification token
-#         verification_token = generate_verification_token(new_user.id)
-
-#         # Create the verification link
-#         verification_link = url_for('verify', token=verification_token, _external=True)
-
-#         # Send the verification email by calling the new function
-#         send_verification_email(new_user, password, verification_link)
-
-#         return jsonify({'status': 'success',
-#                          'message': 'A verification email has been sent to your email address. Please check your inbox.',
-#                          'success_message': 'Account created successfully'
-#                          })
-#     else:
-#         return jsonify({'message': 'You do not have permission to create recruiter accounts.'})
-#--changes here---------------------------------------------------------------
-
-# Configure Flask-Mail for Microsoft Office 365 (Outlook)
-app.config['MAIL_SERVER'] = 'smtp.office365.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = False
-app.config['MAIL_USERNAME'] = 'teja.g@makonissoft.com'  # Your Office 365 email
-app.config['MAIL_PASSWORD'] = 'Mani@9908$'  # Your Office 365 email password (consider using environment variables in production)
-app.config['MAIL_DEFAULT_SENDER'] = 'teja.g@makonissoft.com'  # Default sender address
-app.config['MAIL_REPLY_TO'] = 'teja.g@makonissoft.com'  # Reply-to address
-
-# Initialize Flask-Mail with the app
-mail = Mail(app)
-
-# Enable debug mode for Flask-Mail
-app.config['MAIL_DEBUG'] = True
-
-# Configure logging to capture email sending errors
-logging.basicConfig(level=logging.DEBUG)
-
-# Send Verification Email Function
 def send_verification_email(new_user, password, verification_link):
     html_body = f'''
     <html>
@@ -5028,21 +4871,20 @@ def send_verification_email(new_user, password, verification_link):
     </html>
     '''
 
-    msg = Message('Account Verification', sender=app.config['MAIL_DEFAULT_SENDER'], recipients=[new_user.email])
+    msg = Message('Account Verification', sender=config.sender_email, recipients=[new_user.email])
     msg.html = html_body
     try:
         mail.send(msg)
-        app.logger.info(f"Verification email sent to {new_user.email}")
     except Exception as e:
-        app.logger.error(f"Error sending email to {new_user.email}: {str(e)}")
+        print("mail error", str(e))
         return f'Failed to send mail: {str(e)}'
     return None
 
-# Signup API
+# The signup function
 @app.route('/signup', methods=['POST'])
 def signup():
     data = request.json
-    user_id = data.get('user_id')
+    user_id = data.get('user_id')  # Using get method to avoid KeyError
     user = User.query.filter_by(id=user_id).first()
 
     if not user:
@@ -5092,6 +4934,164 @@ def signup():
                          })
     else:
         return jsonify({'message': 'You do not have permission to create recruiter accounts.'})
+#--changes here---------------------------------------------------------------
+
+# Configure Flask-Mail for Microsoft Office 365 (Outlook)
+# app.config['MAIL_SERVER'] = 'smtp.office365.com'
+# app.config['MAIL_PORT'] = 587
+# app.config['MAIL_USE_TLS'] = True
+# app.config['MAIL_USE_SSL'] = False
+# app.config['MAIL_USERNAME'] = 'teja.g@makonissoft.com'  # Your Office 365 email
+# app.config['MAIL_PASSWORD'] = 'Mani@9908$'  # Your Office 365 email password (consider using environment variables in production)
+# app.config['MAIL_DEFAULT_SENDER'] = 'teja.g@makonissoft.com'  # Default sender address
+# app.config['MAIL_REPLY_TO'] = 'teja.g@makonissoft.com'  # Reply-to address
+
+# # Initialize Flask-Mail with the app
+# mail = Mail(app)
+
+# # Enable debug mode for Flask-Mail
+# app.config['MAIL_DEBUG'] = True
+
+# # Configure logging to capture email sending errors
+# logging.basicConfig(level=logging.DEBUG)
+
+# # Send Verification Email Function
+# def send_verification_email(new_user, password, verification_link):
+#     html_body = f'''
+#     <html>
+#     <head>
+#         <style>
+#             body {{
+#                 font-family: Arial, sans-serif;
+#                 line-height: 1.6;
+#             }}
+#             .container {{
+#                 max-width: 600px;
+#                 margin: auto;
+#                 padding: 20px;
+#                 border: 1px solid #ddd;
+#                 border-radius: 5px;
+#                 box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+#             }}
+#             h2 {{
+#                 color: #333;
+#             }}
+#             p {{
+#                 color: #555;
+#             }}
+#             ul {{
+#                 list-style-type: none;
+#                 padding: 0;
+#             }}
+#             ul li {{
+#                 background: #f9f9f9;
+#                 margin: 5px 0;
+#                 padding: 10px;
+#                 border: 1px solid #ddd;
+#                 border-radius: 3px;
+#             }}
+#             a {{
+#                 color: #1a73e8;
+#                 text-decoration: none;
+#             }}
+#             a:hover {{
+#                 text-decoration: underline;
+#             }}
+#             .footer {{
+#                 margin-top: 20px;
+#                 font-size: 0.9em;
+#                 color: #888;
+#             }}
+#         </style>
+#     </head>
+#     <body>
+#         <div class="container">
+#             <h2>Hello {new_user.name},</h2>
+#             <p>We are pleased to inform you that your account has been successfully created for the <strong>ATS Makonis Talent Track Pro</strong>. Here are your login credentials:</p>
+#             <ul>
+#                 <li><strong>Username:</strong> {new_user.username}</li>
+#                 <li><strong>Password:</strong> {password}</li>
+#             </ul>
+#             <p>Please note that the verification link will expire after 24 hours.</p>
+#             <p>To verify your account, please click on the following link:</p>
+#             <p><a href="{verification_link}">Verify Your Account</a></p>
+#             <p>After successfully verifying your account, you can access the application using the following link:</p>
+#             <p><a href="https://ats-makonis.netlify.app/">Application Link (Post Verification)</a></p>
+#             <p>If you have any questions or need assistance, please feel free to reach out.</p>
+#             <p>Best regards,</p>
+#             <p><strong>ATS Makonis Talent Track Pro Team</strong></p>
+#             <div class="footer">
+#                 <p>This is an automated message, please do not reply.</p>
+#             </div>
+#         </div>
+#     </body>
+#     </html>
+#     '''
+
+#     msg = Message('Account Verification', sender=app.config['MAIL_DEFAULT_SENDER'], recipients=[new_user.email])
+#     msg.html = html_body
+#     try:
+#         mail.send(msg)
+#         app.logger.info(f"Verification email sent to {new_user.email}")
+#     except Exception as e:
+#         app.logger.error(f"Error sending email to {new_user.email}: {str(e)}")
+#         return f'Failed to send mail: {str(e)}'
+#     return None
+
+# # Signup API
+# @app.route('/signup', methods=['POST'])
+# def signup():
+#     data = request.json
+#     user_id = data.get('user_id')
+#     user = User.query.filter_by(id=user_id).first()
+
+#     if not user:
+#         return jsonify({'status': 'error', 'message': 'Invalid user ID or user does not exist.'})
+
+#     user_type = user.user_type
+#     user_name = user.username
+
+#     if user_type == 'management':
+#         username = data.get('username')
+#         name = data.get('name')
+#         email = data.get('email')
+#         user_type = data.get('user_type')
+
+#         # Check if required fields are provided
+#         if not all([username, name, email, user_type]):
+#             return jsonify({'status': 'error', 'message': 'All fields are required'})
+
+#         # Generate a random password
+#         password = generate_random_password()
+#         hashed_password = hashlib.sha256(password.encode()).hexdigest()
+
+#         created_by = user_name
+
+#         existing_user = User.query.filter(or_(User.username == username, User.email == email, User.name == name)).first()
+
+#         if existing_user:
+#             return jsonify({'status': 'error', 'message': 'Account with the same Username, Email, or Name already exists.'})
+
+#         new_user = User(username=username, password=hashed_password, name=name, email=email, user_type=user_type, created_by=created_by)
+        
+#         db.session.add(new_user)
+#         db.session.commit()
+
+#         # Generate a verification token
+#         verification_token = generate_verification_token(new_user.id)
+
+#         # Create the verification link
+#         verification_link = url_for('verify', token=verification_token, _external=True)
+
+#         # Send the verification email by calling the new function
+#         send_verification_email(new_user, password, verification_link)
+
+#         return jsonify({'status': 'success',
+#                          'message': 'A verification email has been sent to your email address. Please check your inbox.',
+#                          'success_message': 'Account created successfully'
+#                          })
+#     else:
+#         return jsonify({'message': 'You do not have permission to create recruiter accounts.'})
 
 
 
